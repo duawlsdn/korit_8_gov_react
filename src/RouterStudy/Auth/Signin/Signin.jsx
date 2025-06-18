f1c9bd7ac13ef1d6842849d5cdee999889b6f42b
duawlsdn <wlsdn893@naver.com>
2025-06-18 17:37:41 +0900

    AuthStudy_Login_Front

--- src/RouterStudy/Auth/Signin/Signin.jsx
+++ src/RouterStudy/Auth/Signin/Signin.jsx
@@ -159,7 +159,11 @@ function Signin(props) {
     }, [inputItems]);
 
     const handleRegisterOnClick = async () => {
-        const url = "http://localhost:8080/api/users";
+        const url = "http://localhost:8080/api/users/login";
+
+        // 컨트롤러 메서드 login
+        // Dto명 LoginDto
+        // POST 요청
 
         let data = {};
 
@@ -172,9 +176,9 @@ function Signin(props) {
 
         try {
             await axios.post(url, data);
-            alert("사용자 등록 완료");
+            alert("로그인 요청 완료");
         } catch(error) {
-            alert("사용자 등록 오류");
+            alert("로그인 오류");
         }
         
     }
